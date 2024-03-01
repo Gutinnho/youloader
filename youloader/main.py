fc9755a2469dd072bf79296553df1d69b846b83d
Gutinnho <glunardi99@gmail.com>
2024-03-01 15:30:58 -0300

    feat(main): Add the logic to retrieve a valid URL from the user

--- youloader/main.py
+++ youloader/main.py
@@ -1,2 +1,34 @@
+import click
+import validators
+
+
+def exiting():
+    click.clear()
+    click.secho("Aborting", bold=True, fg="green")
+    exit()
+
+
+def is_valid_url(url: str):
+    if not isinstance(url, str):
+        raise click.BadParameter(f"The url must be a string")
+    elif not validators.url(url):
+        raise click.BadParameter("Invalid URL")
+    elif not "youtube.com" in url:
+        raise click.BadParameter("The URL must be from YouTube")
+
+    return url
+
+
 def main():
-    print("Temp")
+    while True:
+        click.clear()
+        try:
+            url = click.prompt("Please enter an url")
+            if is_valid_url(url):
+                break
+        except click.BadParameter as e:
+            click.pause(click.style(e, bold=True, fg="red"), err=True)
+        except click.exceptions.Abort:
+            exiting()
+
+    click.echo(f"This is your url: {url}")
