88abf3b02dfc4b454f23f910b29d23356e0c4222
Gutinnho <glunardi99@gmail.com>
2024-03-01 16:02:56 -0300

    feat(downloader): Add a progress bar
    - Add a progress bar during the video download

--- youloader/downloader.py
+++ youloader/downloader.py
@@ -1,7 +1,23 @@
 import click
 from pytube import YouTube
 
+from .config import PROGRESS_BAR_WIDTH
 from .infos import show_infos
+
+
+def progress_bar(chunk, file_handle, bytes_remaining):
+    """Displays a progress bar during download"""
+    current = (filesize - bytes_remaining) / filesize
+    percent = ("{0:.0f}").format(current * 100)
+
+    progress = int(PROGRESS_BAR_WIDTH * current)
+    bar = ("█" * progress) + ("-" * (PROGRESS_BAR_WIDTH - progress))
+
+    print(
+        f"|{bar}| {percent}%",
+        end="\r",
+        flush=True,
+    )
 
 
 def downloader(url: str, output_path: str) -> None:
@@ -16,7 +32,9 @@ def downloader(url: str, output_path: str) -> None:
     - click.BadParameter: If the video is not found
     """
     try:
-        yt = YouTube(url)
+        global filesize
+
+        yt = YouTube(url, on_progress_callback=progress_bar)
 
         click.clear()
         show_infos(yt)
@@ -24,6 +42,7 @@ def downloader(url: str, output_path: str) -> None:
         video = yt.streams.get_highest_resolution()
         if not video:
             raise click.BadParameter("Video not found")
+        filesize = video.filesize
 
         video.download(output_path)
 
